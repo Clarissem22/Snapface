1b1b4b7a4139153e99094a003057bcc929d4f7d6
step31 <cmille749@gmail.com>
2022-04-20 15:54:19 +0200

    snapface

---
+++ src/app/models/face-snap.model.ts
@@ -0,0 +1,24 @@
+export class FaceSnap {
+        title!: string;
+        description!: string;
+        imageUrl!: string;
+        createdDate!: Date;
+        snaps!: number;
+        location?: string;
+      }
+  
+        // export class FaceSnap {
+        //     title: string;
+        //     description: string;
+        //     createdDate: Date;
+        //     snaps: number;
+        //     imageUrl: string;
+            
+        //     constructor(title: string, description: string, imageUrl: string, createdDate: Date, snaps: number) {
+        //       this.title = title;
+        //       this.description = description;
+        //       this.imageUrl = imageUrl;
+        //       this.createdDate = createdDate;
+        //       this.snaps = snaps;
+        //     }
+        //   }
